b9b404d07cd1a4299166029f8fcad33fa7d0e239
OhSeongCheol <dhtmdcjf7@gmail.com>
2020-07-21 21:48:01 +0900

    feat: H2 설정 테스트 추가

--- src/main/java/com/example/demo/PostController.java
+++ src/main/java/com/example/demo/PostController.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @RestController
 public class PostController {
+
+    @Autowired
+    DataSource dataSource;
+
     @RequestMapping("/create")
-    public String create(){
+    public String create() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection.getMetaData().getUserName());
+        System.out.println(connection.getMetaData().getURL());
         return "Success!!";
     }
 }
