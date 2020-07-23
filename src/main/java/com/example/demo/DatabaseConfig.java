37b31eccad97c0fc4e5a7292bf1c0188975a7867
OhSeongCheol <dhtmdcjf7@gmail.com>
2020-07-23 23:17:22 +0900

    feat: Spring Boot 버전 변경
    - H2 설정 문제로 인한 Spring Boot 2.3.1 -> 2.1.6 버전 변경
    - 불필요 로직 삭제
    - DB 테스트용 기본 테이블 추가 로직 추가

--- src/main/java/com/example/demo/DatabaseConfig.java
+++ src/main/java/com/example/demo/DatabaseConfig.java
@@ -3,14 +3,12 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
 
 @Component
 public class DatabaseConfig implements ApplicationRunner {
@@ -18,31 +16,20 @@ public class DatabaseConfig implements ApplicationRunner {
     @Autowired
     DataSource dataSource;
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
-
     @Override
     public void run(ApplicationArguments args) throws SQLException {
         try(Connection connection = dataSource.getConnection()){
             System.out.println(connection.getMetaData().getURL());
             System.out.println(connection.getMetaData().getUserName());
 
-//            String sql = "CREATE TABLE USER3(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY(id))";
-//            String sql = "select * from INFORMATION_SCHEMA.CATALOGS";
-
-//            Statement statement = connection.createStatement();
-//            Boolean ret = statement.execute(sql);
-//            System.out.println("Return : " + ret);
-
-
             Statement statement = connection.createStatement();
-            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, NAME VARCHAR(255), PRIMARY KEY (ID) )";
+            String sql = "CREATE TABLE TESTTABLE(ID INTEGER NOT NULL, VALUE VARCHAR(255), PRIMARY KEY (ID) )";
             statement.executeUpdate(sql);
+
+            String sql2 = "INSERT INTO TESTTABLE VALUES(1, 'value')";
+            statement.execute(sql2);
         } catch (Exception e){
             System.out.println(e);
         }
-
-        jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'saelobi')");
     }
 }
