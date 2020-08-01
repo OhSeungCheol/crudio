c7f057f768c1f31ad4213c5b89c3d058a2998a26
OhSeongCheol <dhtmdcjf7@gmail.com>
2020-08-01 22:50:15 +0900

    feat: Post Controller URL 폼 추가
    POST Lombok 적용했는데, getter/setter 없음. 이유 확인 필요

--- src/main/java/com/example/demo/Post.java
+++ src/main/java/com/example/demo/Post.java
@@ -27,4 +27,44 @@ public class Post {
     }
 
     public Post(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
 }
--- src/main/java/com/example/demo/PostController.java
+++ src/main/java/com/example/demo/PostController.java
@@ -1,28 +1,49 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
 
 @RestController
+@RequestMapping("/post")
 public class PostController {
 
     @Autowired
     private PostRepository postRepository;
 
-    @RequestMapping("/create")
-    public String create() {
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void create() {
         Post post = new Post("title", "content", new Date(), "writer");
         postRepository.save(post);
-        return "Success!!";
     }
 
-    @RequestMapping("/readAll")
+    @RequestMapping(value = "/readAll", method = RequestMethod.GET)
+    @ResponseBody
     public List<Post> readAll(){
         List<Post> postList = postRepository.findAll();
         return postList;
     }
+
+    @RequestMapping(value = "/readOne", method = RequestMethod.GET)
+    @ResponseBody
+    public Post readOne(){
+        return postRepository.findById(0L).orElseGet(Post::new);
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void update(){
+        //
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void delete(){
+        //
+    }
+
 }
