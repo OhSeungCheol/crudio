8af360d8b9c923f984021c86054a2883ae102458
OhSeongCheol <dhtmdcjf7@gmail.com>
2022-06-19 09:29:19 +0900

    rabbitmq 생산, 구독 샘플 추가

---
+++ src/main/java/com/example/demo/rabbitmq/Publish.java
@@ -0,0 +1,19 @@
+package com.example.demo.rabbitmq;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class Publish {
+    final RabbitTemplate rabbitTemplate;
+
+    @Scheduled(fixedRate = 5000)
+    public void sender() {
+        System.out.println("startSend");
+        rabbitTemplate.convertAndSend("exchange", "routingKey", new Object().toString());
+        System.out.println("endSend");
+    }
+}
---
+++ src/main/java/com/example/demo/rabbitmq/Subscribe.java
@@ -0,0 +1,22 @@
+package com.example.demo.rabbitmq;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Subscribe {
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange(name = "exchange", type = ExchangeTypes.TOPIC),
+            value = @Queue(name = "queue"),
+            key = "routingKey")
+    )
+    public void receiver(Object myObject) {
+        System.out.println("startReceive");
+        System.out.println(myObject);
+        System.out.println("endReceive");
+    }
+}
