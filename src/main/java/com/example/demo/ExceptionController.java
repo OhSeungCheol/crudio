a1d16d79219562598409158461e763750f4df58f
OhSeongCheol <dhtmdcjf7@gmail.com>
2022-07-09 01:39:19 +0900

    Vue 새로 고침 에러 설정

---
+++ src/main/java/com/example/demo/ExceptionController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExceptionController implements ErrorController {
+
+    @GetMapping("/error")
+    public String redirectRoot() {
+        return "index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+}
